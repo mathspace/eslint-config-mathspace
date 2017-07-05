b1149867160248e7030aa3dcf85fca3a47c2a53c
kerrin <kenglish@mathspace.com.au>
2017-07-05 13:59:32 +1000

    Remove rules that conflict with prettier
    This removes any rules that eslint-config-prettier specifies may
    cause conflicts with prettier's formatting choices.

--- index.js
+++ index.js
@@ -19,21 +19,10 @@ module.exports = {
     "prettier"
   ],
   "rules": {
-    "arrow-parens" : [0, "as-needed"],
     "flowtype/define-flow-type": "error",
-    "flowtype/object-type-delimiter": ["error", "semicolon"],
     "flowtype/require-valid-file-annotation": [
       2,
       "always"
-    ],
-    "flowtype/semi": "error",
-    "flowtype/space-after-type-colon": [
-      "error",
-      "always"
-    ],
-    "flowtype/space-before-type-colon": [
-      "error",
-      "never"
     ],
     "flowtype/type-id-match": [
       "error",
@@ -42,14 +31,6 @@ module.exports = {
     "flowtype/use-flow-type": "error",
     "import/no-duplicates": "error",
     "no-duplicate-imports": "off",
-    "no-multiple-empty-lines": [
-      "error",
-      {
-        "max": 1,
-        "maxBOF": 0,
-        "maxEOF": 0
-      }
-    ],
     "no-nested-ternary": "off",
     "no-warning-comments": [
       "error",
@@ -59,15 +40,7 @@ module.exports = {
         ]
       }
     ],
-    "quote-props": [
-      "error",
-      "as-needed",
-      {
-        "numbers": true
-      }
-    ],
     "react/no-unused-prop-types": "off",
-    "no-confusing-arrow": "off",
     "prettier/prettier": [
       "error",
       {
